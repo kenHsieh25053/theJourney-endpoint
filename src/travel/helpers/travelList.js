3cfd8a474542e7ba3e52b0eddc7398d8966da403
Ken Hsieh <kenhsieh@Kende-MacBook-Pro.local>
2019-11-26 01:27:14 +0800

    optimize postTravelList func

--- src/travel/helpers/travelList.js
+++ src/travel/helpers/travelList.js
@@ -6,66 +6,93 @@ module.exports = {
 };
 
 async function postTravelList(userId, args) {
-  try {
-    const result = await models.travelList.findByPk(args.id);
+  const id = uuidv4();
+  let data = Object.assign({}, args, {
+    userId
+  });
+  data['id'] = id;
+  const result = await models.travelList.findOrCreate({
+    where: {
+      id: args.id
+    },
+    defaults: data
+  });
 
-    if (!result.id) {
-      const id = uuidv4();
-      let data = Object.assign({}, args, {
-        userId
-      });
-      data['id'] = id;
-      const finalResult = await models.travelList.create(data);
-      return {
-        status: 200,
-        travelList: [{
-          id: finalResult.id,
-          name: finalResult.name,
-          tags: finalResult.tags,
-          stayFrom: finalResult.stayFrom,
-          stayTo: finalResult.stayTo,
-          days: finalResult.days,
-          cost: finalResult.cost,
-          rates: finalResult.rates,
-          likes: finalResult.likes,
-          createdAt: finalResult.createdAt,
-          updatedAt: finalResult.updatedAt,
-          transportation: finalResult.transportation,
-          review: finalResult.review,
-          userId: finalResult.userId
-        }]
-      };
-    } else {
-      await models.travelList.update(args, {
-        where: {
-          id: args.id
-        }
-      });
-      const finalResult = await models.travelList.findByPk(args.id);
-      return {
-        status: 200,
-        travelList: [{
-          id: finalResult.id,
-          name: finalResult.name,
-          tags: finalResult.tags,
-          stayFrom: finalResult.stayFrom,
-          stayTo: finalResult.stayTo,
-          days: finalResult.days,
-          cost: finalResult.cost,
-          rates: finalResult.rates,
-          likes: finalResult.likes,
-          createdAt: finalResult.createdAt,
-          updatedAt: finalResult.updatedAt,
-          transportation: finalResult.transportation,
-          review: finalResult.review,
-          userId: finalResult.userId
-        }]
-      };
-    }
-  } catch (err) {
-    return {
-      status: 500,
-      message: err.message
-    };
+  if (!result[1]) {
+    await models.travelList.update(args, {
+      where: {
+        id: result[0].id
+      }
+    });
+    const updatedResult = await models.travelList.findByPk(result[0].id);
+    return updatedResult;
+  } else {
+    return result[0];
   }
 }
+
+
+// async function postTravelList(userId, args) {
+//   try {
+//     const result = await models.travelList.findByPk(args.id);
+
+//     if (!result.id) {
+//       const id = uuidv4();
+//       let data = Object.assign({}, args, {
+//         userId
+//       });
+//       data['id'] = id;
+//       const finalResult = await models.travelList.create(data);
+//       return {
+//         status: 200,
+//         travelList: [{
+//           id: finalResult.id,
+//           name: finalResult.name,
+//           tags: finalResult.tags,
+//           stayFrom: finalResult.stayFrom,
+//           stayTo: finalResult.stayTo,
+//           days: finalResult.days,
+//           cost: finalResult.cost,
+//           rates: finalResult.rates,
+//           likes: finalResult.likes,
+//           createdAt: finalResult.createdAt,
+//           updatedAt: finalResult.updatedAt,
+//           transportation: finalResult.transportation,
+//           review: finalResult.review,
+//           userId: finalResult.userId
+//         }]
+//       };
+//     } else {
+//       await models.travelList.update(args, {
+//         where: {
+//           id: args.id
+//         }
+//       });
+//       const finalResult = await models.travelList.findByPk(args.id);
+//       return {
+//         status: 200,
+//         travelList: [{
+//           id: finalResult.id,
+//           name: finalResult.name,
+//           tags: finalResult.tags,
+//           stayFrom: finalResult.stayFrom,
+//           stayTo: finalResult.stayTo,
+//           days: finalResult.days,
+//           cost: finalResult.cost,
+//           rates: finalResult.rates,
+//           likes: finalResult.likes,
+//           createdAt: finalResult.createdAt,
+//           updatedAt: finalResult.updatedAt,
+//           transportation: finalResult.transportation,
+//           review: finalResult.review,
+//           userId: finalResult.userId
+//         }]
+//       };
+//     }
+//   } catch (err) {
+//     return {
+//       status: 500,
+//       message: err.message
+//     };
+//   }
+// }
--- src/travel/resolvers/travelList.js
+++ src/travel/resolvers/travelList.js
@@ -10,8 +10,34 @@ export default {
     travelList: async (_, args, {
       user
     }) => {
-      const userId = user.id;
-      return await postTravelList(userId, args);
+      try {
+        const userId = user.id;
+        const finalResult = await postTravelList(userId, args);
+        return {
+          status: 200,
+          travelList: [{
+            id: finalResult.id,
+            name: finalResult.name,
+            tags: finalResult.tags,
+            stayFrom: finalResult.stayFrom,
+            stayTo: finalResult.stayTo,
+            days: finalResult.days,
+            cost: finalResult.cost,
+            rates: finalResult.rates,
+            likes: finalResult.likes,
+            createdAt: finalResult.createdAt,
+            updatedAt: finalResult.updatedAt,
+            transportation: finalResult.transportation,
+            review: finalResult.review,
+            userId: finalResult.userId
+          }]
+        };
+      } catch (err) {
+        return {
+          status: 500,
+          message: err.message
+        };
+      }
     }
   }
 };
